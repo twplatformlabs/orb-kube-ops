e4e41ad78c42da526deb374a62cbb2f6e34dbe24
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-09-07 20:01:10 -0500

    [ncheneweth] bash typo

--- src/scripts/dependencies.sh
+++ src/scripts/dependencies.sh
@@ -3,10 +3,10 @@ InstallDependencies() {
     export SUDO="sudo";
     echo $SUDO
   fi
-  if [ cat /etc/issue | grep Alpine > /dev/null 2>&1 ]; then
-    command -v curl >/dev/null 2>&1 || { $SUDO apk add --no-cache curl }
-    command -v tar >/dev/null 2>&1 || { $SUDO apk add --no-cache tar }
-  elif [ cat /etc/issue | grep Debian > /dev/null 2>&1 || cat /etc/issue | grep Ubuntu > /dev/null 2>&1 ]; then
+  if cat /etc/issue | grep Alpine > /dev/null 2>&1; then
+    command -v curl >/dev/null 2>&1 || { $SUDO apk add --no-cache curl; }
+    command -v tar >/dev/null 2>&1 || { $SUDO apk add --no-cache tar; }
+  elif cat /etc/issue | grep Debian > /dev/null 2>&1 || cat /etc/issue | grep Ubuntu > /dev/null 2>&1; then
     command -v curl >/dev/null 2>&1 || { $SUDO apt -qq install -y curl; }
     command -v tar >/dev/null 2>&1 || { $SUDO apt -qq install -y tar; }
   fi
