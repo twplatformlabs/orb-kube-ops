2e61bab484f4788d2d2f6df61d111de865ea1b1d
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-09-21 16:23:15 -0500

    add helm install

--- src/scripts/install_helm.sh
+++ src/scripts/install_helm.sh
@@ -2,12 +2,15 @@
 
 function install() {
   echo "installing helm ${1}"
-
+  curl -SLO "https://get.helm.sh/helm-${1}-linux-amd64.tar.gz"
+  sudo tar -xf "helm-${1}-linux-amd64.tar.gz"
+  sudo mv -f linux-amd64/helm /usr/local/bin
 }
 
 function install_latest() {
   echo "installing helm latest"
-
+  curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+  bash ./get_helm.sh
 }
 
 echo "preparing to install helm ${VERSION}"
@@ -26,39 +29,4 @@ else
   fi
 fi
 
-
-
-kubectl version --client=true --short=true || { echo "error: invalid kubernetes version"; exit 2; }
-
-
-
-
-VERIFY_CHECKSUM=false
-
-
-  - when:
-      condition: 
-        equal: [ << parameters.helm-version >>, "latest" ]
-      steps:
-        - run:
-            name: install latest version of helm
-            command: |
-              curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-              sudo bash -c "bash ./get_helm.sh"
-  - when:
-      and:
-        - not:
-            matches:
-              pattern: "^latest$"
-              value: << parameters.helm-version >>
-        - not:
-            matches:
-              pattern: "^executor-version$"
-              value: << parameters.helm-version >>
-      steps:
-        - run:
-            name: install helm << parameters.helm-version >>
-            command: |
-              curl -SLO "https://get.helm.sh/helm-<< parameters.helm-version >>-linux-amd64.tar.gz"
-              sudo tar -xf "helm-<< parameters.helm-version >>-linux-amd64.tar.gz"
-              sudo mv -f linux-amd64/helm /usr/local/bin
+kubectl version || { echo "error: invalid helm version"; exit 2; }
