a602c2bf4e6e8286a8555f60be4a59789f413bc4
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-12-29 12:20:45 -0600

    [ncheneweth] debug

--- src/scripts/cis_benchmarks.sh
+++ src/scripts/cis_benchmarks.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
-kubectl apply -n ${NAMESPACE} -f cis-benchmarks-deployment.yaml && sleep 10
+echo "deploy benchmark job"
+kubectl apply -n ${NAMESPACE} -f cis-benchmarks-deployment.yaml
 
+echo "sleep for 10 seconds"
+sleep 10
+
+echo "check job logs for FAILURE"
 kubectl logs -n ${NAMESPACE} -f job.batch/kube-bench --all-containers=true | grep "\[FAIL" > temp.results
+
+echo "eval temp.results file"
 if [[ $(cat temp.results) ]]; then
   echo "kube-bench conformance results error:"
   cat temp.results
   exit 1
 fi
-rm temp.results
 
+echo "delete benchmark job"
 kubectl delete -n ${NAMESPACE} -f cis-benchmarks-deployment.yaml
