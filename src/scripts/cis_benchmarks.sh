c74e58bd5b0d133532b0c07927eaf08f5cff090f
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-12-29 18:09:57 -0600

    [ncheneweth] debug

--- src/scripts/cis_benchmarks.sh
+++ src/scripts/cis_benchmarks.sh
@@ -7,11 +7,10 @@ echo "sleep for 10 seconds"
 sleep 10
 
 echo "check job logs for FAILURE"
-kubectl logs -n ${NAMESPACE} -f job.batch/kube-bench --all-containers=true | grep "\[FAIL" > temp.results
+RESULTS=$(kubectl logs -n ${NAMESPACE} -f job.batch/kube-bench --all-containers=true | grep "0 checks FAIL")
 
 echo "eval temp.results file"
-#if [[ $(cat temp.results) ]]; then
-if [[ -f temp.results ]] && [[ ! -s temp.results ]]; then
+if [[ -z "$RESULTS" ]]; then
   echo "kube-bench conformance results error:"
   cat temp.results
   exit 1
