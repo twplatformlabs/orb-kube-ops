33966baf1392b86adb80fbf97da439366a318fe6
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-08-23 17:17:13 -0500

    [nc] test USE_SUDO logic
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_bats.sh
+++ src/scripts/install_bats.sh
@@ -2,28 +2,29 @@
 
 function install() {
   echo "installing bats ${1}"
-  sudo npm install -g bats@${1}
+  npm install -g bats@${1}
 
 }
 
 function install_latest() {
   echo "installing bats latest"
-  sudo npm install -g bats
+  npm install -g bats
 }
 
 echo "requested bats ${INSTALL_BATS_VERSION}"
+echo "USE_SUDO = ${USE_SUDO}"
 
 if [[ $INSTALL_BATS_VERSION == "latest" ]]; then
-  if [ "$(id -u)" = 0 ]; then
-    install_latest
-  else
+  if [ "$USE_SUDO" == 1 ]; then
     sudo bash -c "$(declare -f install_latest); install_latest;"
+  else
+    install_latest
   fi
 else
-  if [ "$(id -u)" = 0 ]; then
-    install ${INSTALL_BATS_VERSION}
-  else
+  if [ "$USE_SUDO" == 1 ]; then
     sudo bash -c "$(declare -f install); install ${INSTALL_BATS_VERSION};"
+  else
+    install ${INSTALL_BATS_VERSION}
   fi
 fi
 
