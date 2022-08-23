116bd8d598bc9d0372866f34cf7f47f96d20133a
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-08-23 18:34:48 -0500

    [nc] orb-kube-ops testing iteration
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_inspec.sh
+++ src/scripts/install_inspec.sh
@@ -12,20 +12,20 @@ function install_latest() {
 }
 
 echo "requested inspec ${INSTALL_INSPEC_VERSION}"
+echo "USE_SUDO = ${USE_SUDO}"
 
 if [[ $INSTALL_INSPEC_VERSION == "latest" ]]; then
-  if [ "$(id -u)" = 0 ]; then
-    install_latest
-  else
+  if [ "$USE_SUDO" == 1 ]; then
     sudo bash -c "$(declare -f install_latest); install_latest;"
+  else
+    install_latest
   fi
 else
-  if [ "$(id -u)" = 0 ]; then
-    install ${INSTALL_INSPEC_VERSION}
-  else
+  if [ "$USE_SUDO" == 1 ]; then
     sudo bash -c "$(declare -f install); install ${INSTALL_INSPEC_VERSION};"
+  else
+    install ${INSTALL_INSPEC_VERSION}
   fi
 fi
 
 inspec version || { echo "error: invalid inspec version"; exit 2; }
-
