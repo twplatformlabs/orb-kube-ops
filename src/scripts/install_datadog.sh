f661a96fb5d0145891874f9a383010767b548f93
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-08-23 18:23:09 -0500

    [nc] orb-kube-ops testing iteration
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_datadog.sh
+++ src/scripts/install_datadog.sh
@@ -2,28 +2,29 @@
 
 function install() {
   echo "installing datadog ${1}"
-  sudo pip install datadog==${1}
+  pip install datadog==${1}
 
 }
 
 function install_latest() {
   echo "installing datadog latest"
-  sudo pip install datadog
+  pip install datadog
 }
 
 echo "requested datadog ${INSTALL_DATADOG_VERSION}"
+echo "USE_SUDO = ${USE_SUDO}"
 
 if [[ $INSTALL_DATADOG_VERSION == "latest" ]]; then
-  if [ "$(id -u)" = 0 ]; then
-    install_latest
-  else
+  if [ "$USE_SUDO" == 1 ]; then
     sudo bash -c "$(declare -f install_latest); install_latest;"
+  else
+    install_latest
   fi
 else
-  if [ "$(id -u)" = 0 ]; then
-    install ${INSTALL_DATADOG_VERSION}
-  else
+  if [ "$USE_SUDO" == 1 ]; then
     sudo bash -c "$(declare -f install); install ${INSTALL_DATADOG_VERSION};"
+  else
+    install ${INSTALL_DATADOG_VERSION}
   fi
 fi
 
