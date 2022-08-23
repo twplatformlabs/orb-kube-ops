2cf838b0851f057e4a76696d28831fce400a2bbf
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-08-23 17:06:07 -0500

    [nc] test USE_SUDO logic
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_awspec.sh
+++ src/scripts/install_awspec.sh
@@ -2,31 +2,30 @@
 
 function install() {
   echo "installing awspec ${1}"
-  sudo gem install --no-document awspec:${1}
+  gem install --no-document awspec:${1}
 
 }
 
 function install_latest() {
   echo "installing awspec latest"
-  sudo gem install --no-document awspec
+  gem install --no-document awspec
 }
 
 echo "requested awspec ${INSTALL_AWSPEC_VERSION}"
+echo "USE_SUDO = ${USE_SUDO}"
 
 if [[ $INSTALL_AWSPEC_VERSION == "latest" ]]; then
-  if [ "$(id -u)" = 0 ]; then
-    install_latest
-  else
+  if [ "$USE_SUDO" == 1  ]; then
     sudo bash -c "$(declare -f install_latest); install_latest;"
+  else
+    install_latest
   fi
 else
-  if [ "$(id -u)" = 0 ]; then
-    install ${INSTALL_AWSPEC_VERSION}
-  else
+  if [ "$USE_SUDO" == 1  ]; then
     sudo bash -c "$(declare -f install); install ${INSTALL_AWSPEC_VERSION};"
+  else
+    install ${INSTALL_AWSPEC_VERSION}
   fi
 fi
 
 awspec version || { echo "error: invalid inspec version"; exit 2; }
-
-             
