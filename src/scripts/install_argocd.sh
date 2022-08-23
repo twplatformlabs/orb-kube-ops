c85ae378cee4b0ff651e0656ddf5e1ea28e97bf3
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-08-23 17:03:13 -0500

    [nc] test USE_SUDO logic
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_argocd.sh
+++ src/scripts/install_argocd.sh
@@ -4,29 +4,30 @@ function install() {
   echo "installing argocd ${1}"
   curl -sSL -o argocd https://github.com/argoproj/argo-cd/releases/download/v${1}/argocd-linux-amd64
   chmod +x argocd
-  mv ./argocd /usr/local/bin/argocd
+  mv -f argocd /usr/local/bin/argocd
 }
 
 function install_latest() {
   echo "installing argocd latest"
   curl -sSL -o argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
   chmod +x argocd
-  mv ./argocd /usr/local/bin/argocd
+  mv -f argocd /usr/local/bin/argocd
 }
 
 echo "requested argocd ${INSTALL_ARGOCD_VERSION}"
+echo "USE_SUDO = ${USE_SUDO}"
 
 if [[ $INSTALL_ARGOCD_VERSION == "latest" ]]; then
   if [ "$USE_SUDO" == 1 ]; then
-    install_latest
-  else
     sudo bash -c "$(declare -f install_latest); install_latest;"
+  else
+    install_latest
   fi
 else
   if [ "$USE_SUDO" == 1 ]; then
-    install ${INSTALL_ARGOCD_VERSION}
-  else
     sudo bash -c "$(declare -f install); install ${INSTALL_ARGOCD_VERSION};"
+  else
+    install ${INSTALL_ARGOCD_VERSION}
   fi
 fi
 
