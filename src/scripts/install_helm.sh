4ff65de91e435b9fe6c6e5769d6559e2815d3a80
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-08-23 18:29:27 -0500

    [nc] orb-kube-ops testing iteration
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_helm.sh
+++ src/scripts/install_helm.sh
@@ -3,8 +3,8 @@
 function install() {
   echo "installing helm ${1}"
   curl -SLO "https://get.helm.sh/helm-v${1}-linux-amd64.tar.gz"
-  sudo tar -xf "helm-v${1}-linux-amd64.tar.gz"
-  sudo mv -f linux-amd64/helm /usr/local/bin
+  tar -xf "helm-v${1}-linux-amd64.tar.gz"
+  mv -f linux-amd64/helm /usr/local/bin
 }
 
 function install_latest() {
@@ -14,18 +14,19 @@ function install_latest() {
 }
 
 echo "requested helm ${INSTALL_HELM_VERSION}"
+echo "USE_SUDO = ${USE_SUDO}"
 
 if [[ $INSTALL_HELM_VERSION == "latest" ]]; then
-  if [ "$(id -u)" = 0 ]; then
-    install_latest
-  else
+  if [ "$USE_SUDO" == 1 ]; then
     sudo bash -c "$(declare -f install_latest); install_latest;"
+  else
+    install_latest
   fi
 else
-  if [ "$(id -u)" = 0 ]; then
-    install ${INSTALL_HELM_VERSION}
-  else
+  if [ "$USE_SUDO" == 1 ]; then
     sudo bash -c "$(declare -f install); install ${INSTALL_HELM_VERSION};"
+  else
+    install ${INSTALL_HELM_VERSION}
   fi
 fi
 
