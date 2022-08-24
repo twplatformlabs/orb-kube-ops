4666891acbde61302ccccf30b6a8642b4c4a0027
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-08-24 10:49:53 -0500

    [nc] orb-kube-ops testing iteration
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_istioctl.sh
+++ src/scripts/install_istioctl.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-echo "requested kubectl ${INSTALL_ISTIOCTL_VERSION}"
+function install() {
+  echo "installing istioctl ${1}"
+  curl -L https://istio.io/downloadIstio  | ISTIO_VERSION="${1}" sh -
+  mv -f "istio*/bin/istioctl" /usr/local/bin/istioctl
+}
 
-curl -L https://istio.io/downloadIstio  | ISTIO_VERSION="${INSTALL_ISTIOCTL_VERSION}" sh -
-sudo mv -f "istio-${INSTALL_ISTIOCTL_VERSION}/bin/istioctl" /usr/local/bin/istioctl 
+function install_latest() {
+  echo "installing istioctl latest"
+  curl -L https://istio.io/downloadIstio  | sh -
+  mv -f "istio*/bin/istioctl" /usr/local/bin/istioctl
+}
+
+echo "requested istioctl ${INSTALL_ISTIOCTL_VERSION}"
+echo "USE_SUDO = ${USE_SUDO}"
+
+if [[ $INSTALL_ISTIOCTL_VERSION == "latest" ]]; then
+  if [ "$USE_SUDO" == 1 ]; then
+    sudo bash -c "$(declare -f install_latest); install_latest;"
+  else
+    install_latest
+  fi
+else
+  if [ "$USE_SUDO" == 1 ]; then
+    sudo bash -c "$(declare -f install); install ${INSTALL_ISTIOCTL_VERSION};"
+  else
+    install ${INSTALL_ISTIOCTL_VERSION}
+  fi
+fi
 
 istioctl version --short --remote=false || { echo "error: invalid istioctl version"; exit 2; }
