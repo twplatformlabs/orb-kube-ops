f5c7a07a9a9ff34c96ee2c8b39283d89a0eceff6
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-08-24 14:04:02 -0500

    [nc] add kind package
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

---
+++ src/scripts/install_kind.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function install() {
+  echo "installing kind ${1}"
+  curl -Lo ./kind https://kind.sigs.k8s.io/dl/v${1}/kind-linux-amd64
+  chmod +x ./kind
+  mv ./kind /usr/local/bin/kind
+}
+
+function install_latest() {
+  echo "installing kind latest"
+  curl -Lo ./kind https://kind.sigs.k8s.io/dl/latest/kind-linux-amd64
+  chmod +x ./kind
+  mv ./kind /usr/local/bin/kind
+}
+
+echo "requested kind ${INSTALL_KIND_VERSION}"
+echo "USE_SUDO = ${USE_SUDO}"
+
+if [[ $INSTALL_KIND_VERSION == "latest" ]]; then
+  if [ "$USE_SUDO" == 1 ]; then
+    sudo bash -c "$(declare -f install_latest); install_latest;"
+  else
+    install_latest
+  fi
+else
+  if [ "$USE_SUDO" == 1 ]; then
+    sudo bash -c "$(declare -f install); install ${INSTALL_KIND_VERSION};"
+  else
+    install ${INSTALL_KIND_VERSION}
+  fi
+fi
+
+kind version || { echo "error: invalid kind version"; exit 2; }
