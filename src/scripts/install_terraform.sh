d3831bd5efe5f8b6a1e1659cb8a27df110e9a488
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-08-24 11:33:15 -0500

    [nc] orb-kube-ops testing iteration
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_terraform.sh
+++ src/scripts/install_terraform.sh
@@ -2,8 +2,8 @@
 
 function install() {
   echo "installing terraform ${1}"
-  curl -SLO "https://releases.hashicorp.com/terraform/${INSTALL_TERRAFORM_VERSION}/terraform_${INSTALL_TERRAFORM_VERSION}_linux_amd64.zip" > "terraform_${INSTALL_TERRAFORM_VERSION}_linux_amd64.zip"
-  unzip "terraform_${INSTALL_TERRAFORM_VERSION}_linux_amd64.zip" -d /usr/local/bin
+  curl -SLO "https://releases.hashicorp.com/terraform/${1}/terraform_${1}_linux_amd64.zip" > "terraform_${1}_linux_amd64.zip"
+  unzip "terraform_${1}_linux_amd64.zip" -d /usr/local/bin
 }
 
 echo "requested terraform ${INSTALL_TERRAFORM_VERSION}"
