d150006e8633063206b1ac5880c5d92723d0db90
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-09-24 20:52:53 -0500

    [nc] fix test error
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_karmada.sh
+++ src/scripts/install_karmada.sh
@@ -2,11 +2,11 @@
 set -eo pipefail
 
 function install() {
-  echo "installing karmada ${1}"
+  echo "installing karmadactl ${1}"
   curl -s https://raw.githubusercontent.com/karmada-io/karmada/master/hack/install-cli.sh | INSTALL_CLI_VERSION=${1} bash
 }
 
-echo "requested Karmada ${INSTALL_KARMADA_VERSION}"
+echo "requested Karmadactl ${INSTALL_KARMADA_VERSION}"
 echo "USE_SUDO = ${USE_SUDO}"
 
 if [[ "$INSTALL_KARMADA_VERSION" == "latest" ]]; then
@@ -14,7 +14,7 @@ if [[ "$INSTALL_KARMADA_VERSION" == "latest" ]]; then
   exit 1
 fi
 
-echo "preparing to install karmada ${VERSION}"
+echo "preparing to install karmadactl ${VERSION}"
 
 if [[ "$USE_SUDO" == "true" ]]; then
   sudo bash -c "$(declare -f install); install ${VERSION};"
@@ -22,6 +22,6 @@ else
   install "${VERSION}"
 fi
 
-karmada version || { echo "error: invalid karmada version"; exit 2; }
+karmadactl version || { echo "error: invalid karmadactl version"; exit 2; }
 
 
