ec7c78977d9c5adbff1eddd7e98acc835b89ea48
Nic Cheneweth <nchenewe@thoughtworks.com>
2026-05-05 11:16:55 -0500

    [nc] iterating on integration tests
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_trivy.sh
+++ src/scripts/install_trivy.sh
@@ -3,23 +3,21 @@ set -eo pipefail
 
 function install() {
   echo "installing trivy ${1}"
-  curl -SLO "https://github.com/aquasecurity/trivy/releases/download/v${1}/trivy_${1}_Linux-64bit.tar.gz"
-  tar -xzf "trivy_${1}_Linux-64bit.tar.gz"
-  mv trivy /usr/local/bin/trivy
+  curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin "${1}"
 }
 
 echo "requested trivy ${INSTALL_TRIVY_VERSION}"
 echo "USE_SUDO = ${USE_SUDO}"
 
+# set to empty string if latest
 if [[ "$INSTALL_TRIVY_VERSION" == "latest" ]]; then
-  echo "install of 'latest' not supported"
-  exit 1
+  export INSTALL_TRIVY_VERSION=""
+fi
+
+if [ "$USE_SUDO" == "true" ]; then
+  sudo bash -c "$(declare -f install); install ${INSTALL_TRIVY_VERSION};"
 else
-  if [ "$USE_SUDO" == "true" ]; then
-    sudo bash -c "$(declare -f install); install ${INSTALL_TRIVY_VERSION};"
-  else
-    install "${INSTALL_TRIVY_VERSION}"
-  fi
+  install "${INSTALL_TRIVY_VERSION}"
 fi
 
 trivy version || { echo "error: invalid trivy version"; exit 2; }
